b713bfb5e12ff51a217ffe8f6b44c7d06cf46de2
Rajattiwari736 <rajattiwari736@gmail.com>
2024-12-08 12:21:45 +0000

    New Basics file

---
+++ Basics/01_Basics.js
@@ -0,0 +1,7 @@
+const account_Id=123 // when i want the value not to be changed by any user
+let account_Name="tyagi.shailja@gmail.com" //use let since it helps in variable scoping
+var account_password="125" //var is not used now a days due to block scope and functional scope issues
+account_city="jaipur" //not recommended
+
+
+console.table([account_Id,account_Name,account_password,account_city]); //can use or cannot use semicolon
---
+++ Basics/01_dataTypes.js
@@ -0,0 +1,39 @@
+"use strict"; // we write this to tell the compiler that we are going to use newer syntax
+
+// Primitive dataType
+// Number
+// Bigint
+// string
+// symbol=>used  for uniqueness 
+// boolean
+// null ==> when we used the value null it is a type of OBJECT
+// undefined ==>when we used the value undefined , it returns undefined
+
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+
+//Non Primitive DataType
+//object
+
+// https://tc39.es/ecma262/
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
+
